483c96660d1cedac3f605ca74983829998ccc319
zhihai.zhu <zhihai.zhu@monstar-lab.com>
2016-10-26 14:03:57 +0800

    siri

---
+++ Forget_SiriUI/IntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntentViewController.h
+//  Forget_SiriUI
+//
+//  Created by 111 on 16/10/26.
+//  Copyright © 2016年 叶同学. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
