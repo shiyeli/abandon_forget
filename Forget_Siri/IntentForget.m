22878b0921a9173f7d48648ece347e5216b8ac7e
zhihai.zhu <zhihai.zhu@monstar-lab.com>
2016-10-27 16:48:15 +0800

    单独创建intent forget

---
+++ Forget_Siri/IntentForget.m
@@ -0,0 +1,13 @@
+//
+//  IntentForget.m
+//  Forget_iOS
+//
+//  Created by 111 on 16/10/27.
+//  Copyright © 2016年 叶同学. All rights reserved.
+//
+
+#import "IntentForget.h"
+
+@implementation IntentForget
+
+@end
