cac52e6f5b5a0b3e37d731e4b168a9c5ed7d5635
zhihai.zhu <zhihai.zhu@monstar-lab.com>
2016-10-27 17:06:52 +0800

    ss

--- Forget_Siri/IntentHandler.m
+++ Forget_Siri/IntentHandler.m
@@ -27,7 +27,7 @@
 @implementation IntentHandler
 
 - (id)handlerForIntent:(INIntent *)intent {
-    return [[IntentForget alloc] init];
+//    return [[IntentForget alloc] init];
     
     // This is the default implementation.  If you want different objects to handle different intents,
     // you can override this and return the handler you want for that particular intent.
