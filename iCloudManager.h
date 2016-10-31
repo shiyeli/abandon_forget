8d898394673f05a7c08d7132e8f3e9bf0557f935
zhihai.zhu <zhihai.zhu@monstar-lab.com>
2016-10-31 11:56:21 +0800

    sss

---
+++ iCloudManager.h
@@ -0,0 +1,13 @@
+//
+//  iCloudManager.h
+//  Forget_iOS
+//
+//  Created by 111 on 16/10/31.
+//  Copyright © 2016年 叶同学. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iCloudManager : NSObject
+
+@end
---
+++ iCloudManager.m
@@ -0,0 +1,16 @@
+//
+//  iCloudManager.m
+//  Forget_iOS
+//
+//  Created by 111 on 16/10/31.
+//  Copyright © 2016年 叶同学. All rights reserved.
+//
+
+#import "iCloudManager.h"
+#import <CloudKit/CloudKit.h>
+@implementation iCloudManager
++ (void)load{
+    
+}
+
+@end
